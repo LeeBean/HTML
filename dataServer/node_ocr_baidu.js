958e30f0f3305e95c990d5b508649f4e0dd6de4d
LLB <214434802@qq.com>
2018-01-09 00:50:08 +0800

    sss

---
+++ dataServer/node_ocr_baidu.js
@@ -0,0 +1,32 @@
+var AipOcrClient = require("baidu-aip-sdk").ocr;
+
+// 设置APPID/AK/SK
+// var APP_ID = "10643899";
+// var API_KEY = "Pj3mgnFIUjjQFRBSzp0dWPdN";
+// var SECRET_KEY = "ihnw9zf01kem2Mjq3cLSqyNR9TTNmYwI";
+
+// 设置APPID/AK/SK
+var APP_ID = "10658111";
+var API_KEY = "9c0N81MAe5WmaMQBdnB7vK86";
+var SECRET_KEY = "qrFWIx7K00bPSjTUqYf4hGo2QwE9PMOQ";
+
+var client = new AipOcrClient(APP_ID, API_KEY, SECRET_KEY);
+
+var fs = require('fs');
+
+module.exports = {
+    getResult: function(res, imgName) {;
+        var image = fs.readFileSync(imgName);
+        var base64Img = new Buffer(image).toString('base64');
+        var options = {
+            recognize_granularity: 'small'
+        }
+        client.general(base64Img, options).then(function(result) {
+            console.log(JSON.stringify(result));
+            res.json(result);
+        }).catch(function(err) {
+            // 如果发生网络错误
+            console.log(err);
+        });
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
+var formidable = require('formidable');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.route("/uploadPhoto").get(function(req, res) { // 到达此路径则渲染register文件，并传出title值供 register.html使用
+    res.render("uploadPhoto", { title: '图片文字识别', message: "" });
+}).post(function(req, res) {
+    console.log(1111);
+    // 跨域
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
 
+    let form = new formidable.IncomingForm();
+    form.encoding = 'utf-8'; // 编码
+    form.keepExtensions = true; // 保留扩展名
+    form.maxFieldsSize = 10 * 1024 * 1024; // 文件大小
+    form.uploadDir = '/Users/llb/Documents/code/HTML/assets/OCR/' // 存储路径
+    form.parse(req, function(err, fileds, files) { // 解析 formData数据
+        if (err) { return console.log(err) }
+
+        let imgPath = files.img.path // 获取文件路径
+        let imgName = "/Users/llb/Documents/code/HTML/assets/OCR/test." + files.img.type.split("/")[1] // 修改之后的名字
+        let data = fs.readFileSync(imgPath) // 同步读取文件
+
+        fs.writeFile(imgName, data, function(err) { // 存储文件
+            if (err) { return console.log(err) }
+
+            fs.unlink(imgPath, function() {}) // 删除文件
+                //上传图片成功返回code:1
+                //res.json({code:1})
+            global.nodeServer.getResult(res, imgName);
+        })
+    });
+});
 module.exports = router;
